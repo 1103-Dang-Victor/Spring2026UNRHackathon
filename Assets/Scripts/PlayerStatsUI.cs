1d49a12f3d77feeb17ee5938dfff6496048eab01
Hyphenix <itsvictordang@gmail.com>
2026-04-11 13:32:42 -0700

    attaching releavnt components

--- Assets/Scripts/PlayerStatsUI.cs
+++ Assets/Scripts/PlayerStatsUI.cs
@@ -10,7 +10,7 @@ public class PlayerStatsUI : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerStatsText.text = "=========================";
+        playerStatsText.text = "======================";
     }
 
     // Update is called once per frame
